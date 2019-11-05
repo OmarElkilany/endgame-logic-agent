7f5e308a4e5c87c8de2984d39adb1f1b9a64aa2e
OmarElkilany <35930449+OmarElkilany@users.noreply.github.com>
2019-11-05 19:43:24 +0200

    Update Endgame.pl

--- Endgame.pl
+++ Endgame.pl
@@ -1,25 +1,25 @@
 :- discontiguous ironMan/3.
 :- discontiguous stone/3.
-:- include('./KB.pl').
+:- include('./KB1.pl').
 
 ironMan(X, Y, result(A, S)):-
     ironMan(OldX, OldY, S),
     grid(M,N),
     (
         (
+            (A = collect, stone(OldX, OldY, S)), X = OldX, Y = OldY
+        );
+        (
             (
                 A = right, OldX = X, Y is OldY + 1, Y < M;
-                A = left, OldX = X, Y is OldY - 1, Y >= 0;
                 A = up, OldY = Y, X is OldX - 1, X >= 0;
+                A = left, OldX = X, Y is OldY - 1, Y >= 0;
                 A = down, OldY = Y, X is OldX + 1, X < N
             ),
             (
                 \+ thanos(X, Y);
                 \+ stone(_, _, S)
             )
-        );
-        (
-            (A = collect, stone(OldX, OldY, S)), X = OldX, Y = OldY
         )
     ).
 
@@ -28,18 +28,5 @@ stone(X, Y, result(A, S)):-
         stone(X, Y, S), (A = right ; A = left; A = up; A = down; A = collect, \+ ironMan(X, Y, S))
     ).
 
-snappedHelper(S):-
-    S = result(snap, Sp), thanos(X, Y), ironMan(X, Y, Sp).
-
-snappedHelperTwo(S, DepthLimit):-
-    (
-        call_with_depth_limit(snappedHelper(S), DepthLimit, Depth),
-        Depth \= depth_limit_exceeded
-    );
-    (
-        NewDepthLimit is DepthLimit + 50,
-        snappedHelperTwo(S, NewDepthLimit)
-    ).
-
 snapped(S):-
-    snappedHelperTwo(S, 200).
+    S = result(snap, Sp), thanos(X, Y), ironMan(X, Y, Sp).
